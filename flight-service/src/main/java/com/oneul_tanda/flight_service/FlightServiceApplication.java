a7b5911ff93f10fbf5925cabdf10fa24819899db
oyeonjoo <oyj0842@naver.com>
2025-04-08 15:43:43 +0900

    initial commit

---
+++ flight-service/src/main/java/com/oneul_tanda/flight_service/FlightServiceApplication.java
@@ -0,0 +1,15 @@
+package com.oneul_tanda.flight_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class FlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightServiceApplication.class, args);
+	}
+
+}
