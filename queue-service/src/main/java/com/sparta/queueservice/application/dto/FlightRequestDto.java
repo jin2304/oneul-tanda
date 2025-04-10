0aac70f7290628b735e88818ba1094511c904e3d
earthkid0 <ghs1541@naver.com>
2025-04-10 13:14:14 +0900

    fix/불필요한 setter 삭제

--- queue-service/src/main/java/com/sparta/queueservice/application/dto/FlightRequestDto.java
+++ queue-service/src/main/java/com/sparta/queueservice/application/dto/FlightRequestDto.java
@@ -1,10 +1,8 @@
 package com.sparta.queueservice.application.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class FlightRequestDto {
     private String flightId;
     private int seatCount;
