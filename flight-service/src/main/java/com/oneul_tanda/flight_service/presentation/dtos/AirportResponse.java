73f755718b7dbfcefccc56ab1f62d63e7b497792
oyeonjoo <oyj0842@naver.com>
2025-04-12 17:09:30 +0900

    move: resDto presentation 레이어로 이동

--- flight-service/src/main/java/com/oneul_tanda/flight_service/presentation/dtos/AirportResponse.java
+++ flight-service/src/main/java/com/oneul_tanda/flight_service/presentation/dtos/AirportResponse.java
@@ -1,4 +1,4 @@
-package com.oneul_tanda.flight_service.application.dtos;
+package com.oneul_tanda.flight_service.presentation.dtos;
 
 import com.oneul_tanda.flight_service.domain.entity.Airport;
 import java.util.UUID;
