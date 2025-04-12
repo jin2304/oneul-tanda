9fcd67d412c13fe4f94f0d53a60f7cef431be39b
jin2304 <search2304@naver.com>
2025-04-13 07:36:03 +0900

    feat: 예약 확정(예약 수정) api 및 dto 추가

---
+++ reservation-service/src/main/java/com/oneul_tanda/reservation_service/reservation/presentation/dto/response/update/ConfirmReservationResponseDto.java
@@ -0,0 +1,37 @@
+package com.oneul_tanda.reservation_service.reservation.presentation.dto.response.update;
+
+import com.oneul_tanda.reservation_service.reservation.domain.entity.Reservation;
+import com.oneul_tanda.reservation_service.reservation.domain.entity.ReservationStatus;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Builder
+public record ConfirmReservationResponseDto(
+        UUID reservationId,
+        Long userId,
+        BigDecimal totalPrice,
+        ReservationStatus status,
+        List<ConfirmTicketResponseDto> tickets
+) {
+
+    // Entity -> DTO 변환 메서드
+    public static ConfirmReservationResponseDto from(Reservation reservation) {
+        return ConfirmReservationResponseDto.builder()
+                .reservationId(reservation.getId())
+                .userId(reservation.getUserId())
+                .totalPrice(reservation.getTotalPrice())
+                .status(reservation.getStatus())
+                .tickets(reservation.getTicketList().stream()
+                        .map(ConfirmTicketResponseDto::from)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+
+
+
+}
