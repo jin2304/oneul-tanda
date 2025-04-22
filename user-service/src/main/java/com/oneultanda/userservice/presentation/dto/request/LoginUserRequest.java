40c5a8f336dfbc98185938d5f71ab16760c9fed0
kss <kos7114@naver.com>
2025-04-22 12:47:03 +0900

    feat: http test, validation
    requestdto에 대한 validation + http test code 일부

--- user-service/src/main/java/com/oneultanda/userservice/presentation/dto/request/LoginUserRequest.java
+++ user-service/src/main/java/com/oneultanda/userservice/presentation/dto/request/LoginUserRequest.java
@@ -1,9 +1,13 @@
 package com.oneultanda.userservice.presentation.dto.request;
 
 import com.oneultanda.userservice.application.dto.comand.LoginUserCommand;
+import jakarta.validation.constraints.NotNull;
 
 public record LoginUserRequest(
+
+        @NotNull(message = "계정명은 필수입니다.")
         String username,
+        @NotNull(message = "비밀번호는 필수입니다.")
         String password
 ) {
     public LoginUserCommand toCommand() {
