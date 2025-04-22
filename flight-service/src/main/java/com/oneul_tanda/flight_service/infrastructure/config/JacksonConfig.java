f8c0f1c3926b8d41682d4ed36f25bf8e978b9448
oyeonjoo <oyj0842@naver.com>
2025-04-22 13:19:39 +0900

    refactor: dto별 직렬화 설정 및 다형성 타입 처리 옵션 제거

--- flight-service/src/main/java/com/oneul_tanda/flight_service/infrastructure/config/JacksonConfig.java
+++ flight-service/src/main/java/com/oneul_tanda/flight_service/infrastructure/config/JacksonConfig.java
@@ -3,8 +3,6 @@ package com.oneul_tanda.flight_service.infrastructure.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +12,11 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
-                .builder()
-                .allowIfSubType(Object.class)
-                .build();
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());// Java 8 날짜 모듈 등록
-        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601로 출력
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 알 수 없는 속성 무시
+
         return objectMapper;
     }
 }
