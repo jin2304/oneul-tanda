8d3200f8c99e3bdd169bbc97190b872aaf73a29a
earthkid0 <ghs1541@naver.com>
2025-04-24 12:05:03 +0900

    refactor/controller response 변경

--- payment-service/src/main/java/com/sparta/paymentservice/web/PaymentController.java
+++ payment-service/src/main/java/com/sparta/paymentservice/web/PaymentController.java
@@ -1,15 +1,12 @@
 package com.sparta.paymentservice.web;
 
-import com.siot.IamportRestClient.response.IamportResponse;
-import com.siot.IamportRestClient.response.Payment;
+import com.sparta.paymentservice.application.dto.PaymentRequestDto;
+import com.sparta.paymentservice.application.dto.PaymentResponseDto;
 import com.sparta.paymentservice.application.service.ImPortPaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,21 +17,10 @@ public class PaymentController {
     private final ImPortPaymentService imPortPaymentService;
 
     @PostMapping("/test")
-    public ResponseEntity<String> requestTestPayment(@RequestParam UUID reservationId,
-                                                     @RequestParam Integer amount) {
-        try {
-            IamportResponse<Payment> response = imPortPaymentService.requestTestPayment(reservationId, amount);
+    public ResponseEntity<PaymentResponseDto> requestTestPayment(@RequestBody PaymentRequestDto request) {
 
-            if (response.getResponse() != null && "paid".equals(response.getResponse().getStatus())) {
-                return ResponseEntity.ok("결제 성공: " + response.getResponse().getApplyNum());
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body("결제 실패: " + response.getMessage());
-            }
+     PaymentResponseDto response = imPortPaymentService.confirmPayment(request);
 
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("결제 요청 중 오류 발생: " + e.getMessage());
-        }
+     return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
