09f027588f27f888b65e09b0261ff0cb34b12af3
jin2304 <search2304@naver.com>
2025-04-30 19:34:06 +0900

    fix: 예약 Kafka 컨슈머 DLQ 설정
    - 예약 Kafka 컨슈머에서 역직렬화, 런타임 에러로 인한 무한 재시도 문제 해결
    - Deserializer 구성: 기존 단일 JsonDeserializer -> ErrorHandlingDeserializer로 래핑하여 예외 발생 시 감지 가능하게 구성
    - DLQ용 KafkaTemplate 설정: 실패 메시지를 reservation-held-dlq 토픽으로 전송할 수 있도록 KafkaTemplate<String, byte[]> 구성
    - DeadLetterPublishingRecoverer + DefaultErrorHandler로 DLQ 전송 처리

--- reservation-service/src/main/java/com/oneul_tanda/reservation_service/reservation/infrastructure/kafka/event/ReservationHeldEvent.java
+++ reservation-service/src/main/java/com/oneul_tanda/reservation_service/reservation/infrastructure/kafka/event/ReservationHeldEvent.java
@@ -1,20 +1,28 @@
 package com.oneul_tanda.reservation_service.reservation.infrastructure.kafka.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ReservationHeldEvent(
-        String eventId,                    // 대상: 이벤트 고유 ID or 이벤트 대상 ID
-        String eventType,                  // 행위: 이벤트 타입    (ex: 예약 선점)
-        LocalDateTime occurrenceDateTime,  // 시간: 행위 발생 시각  (ex: 선점이 일어난 시간)
-        Data data                          // 정보: 행위와 관련된 정보
-) {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationHeldEvent {
+    private UUID eventId;                   // 대상: 이벤트 고유 ID or 이벤트 대상 ID
+    private String eventType;               // 행위: 이벤트 타입    (ex: 예약 선점)
+    private LocalDateTime reservationTime;  // 시간: 행위 발생 시각  (ex: 선점이 일어난 시간)
+    private Data data;                      // 정보: 행위와 관련된 정보
 
-    public record Data(
-            UUID flightId,
-            UUID userId,
-            int seatCount
-    ) {}
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
+        private UUID flightId;
+        private UUID userId;
+        private Integer seatCount;
+    }
 }
 
