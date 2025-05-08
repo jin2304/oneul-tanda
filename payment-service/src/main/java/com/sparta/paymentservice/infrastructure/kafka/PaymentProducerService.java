573e4ae70e91f669948a3709167e575a0e9cec50
earthkid0 <ghs1541@naver.com>
2025-05-08 16:31:19 +0900

    feat/producer 이벤트 메세지

---
+++ payment-service/src/main/java/com/sparta/paymentservice/infrastructure/kafka/PaymentProducerService.java
@@ -0,0 +1,23 @@
+package com.sparta.paymentservice.infrastructure.kafka;
+
+import com.sparta.paymentservice.infrastructure.kafka.event.ReservationCanceledEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PaymentProducerService {
+    private  final KafkaTemplate<String, ReservationCanceledEvent> kafkaTemplate;
+
+    public void sendPaymentCanceled(UUID reservationId, UUID flightId, int seatCount) {
+        ReservationCanceledEvent event = ReservationCanceledEvent
+                .createReservationCanceledEvent(reservationId, flightId, seatCount, "payment-cancelled");
+
+        kafkaTemplate.send("payment-cancelled", reservationId.toString(), event);
+    }
+}
